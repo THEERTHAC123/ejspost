da1e5306f8337cb44a032cb95808bdac2c69cff2
THEERTHAC123 <theertha@smectechnologies.com>
2023-06-28 08:57:38 +0530

    craete post using ejs

---
+++ routes/view.route.js
@@ -0,0 +1,17 @@
+// const express = require("express");
+// const router = express.Router();
+
+// router.get("/", async (req, res) => {
+//   res.render('index');
+// });
+
+// router.get("/add-post", async (req, res) => {
+//     res.render('add-post');
+//   });
+
+//   router.get("/post", async (req, res) => {
+//     res.render('posts');
+//   });
+
+  
+//   module.exports = router;
